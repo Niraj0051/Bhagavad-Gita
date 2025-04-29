77fdbf73b7fc1e509f98c1e10a7a41bc7abc42ae
Niraj0051 <nirajpingale902894@gmail.com>
2025-04-29 19:55:02 +0530

    utility added

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import SlokPage from './pages/SlokPage'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import About from './pages/About'
+import ScrollToTop from './utility/ScrollToTop'
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-blue-50 to-indigo-100">
+      <ScrollToTop />
       <Routes>
         {/* Home route without header/footer */}
         <Route path="/" element={<HomePage />} />
