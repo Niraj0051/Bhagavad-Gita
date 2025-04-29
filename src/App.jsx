f561db9c3e63fdc917bdb1e3c40751382261bad9
Niraj0051 <nirajpingale902894@gmail.com>
2025-04-29 17:00:52 +0530

    Metadata

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,3 @@
-// import { Routes, Route } from 'react-router-dom'
-// import HomePage from './pages/HomePage'
-// // import ChaptersPage from './pages/ChaptersPage'
-// // import ChapterDetailPage from './pages/ChapterDetailPage'
-// // import SlokPage from './pages/SlokPage'
-// import Navbar from './components/Navbar'
-// import Footer from './components/Footer'
-
-// function App() {
-//   return (
-//     <div className="min-h-screen flex flex-col bg-gradient-to-b from-blue-50 to-indigo-100">
-//       <Navbar />
-//       <main className="flex-grow">
-//         <Routes>
-//           <Route path="/" element={<HomePage />} />
-//           {/* <Route path="/chapters" element={<ChaptersPage />} />
-//           <Route path="/chapter/:id" element={<ChapterDetailPage />} />
-//           <Route path="/slok/:chapter/:verse" element={<SlokPage />} /> */}
-//         </Routes>
-//       </main>
-//       <Footer />
-//     </div>
-//   )
-// }
-
-// export default App
-
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ChaptersPage from './pages/ChaptersPage'
